170d244c3fee492e5b40b8c2f4e0278a1044c8af
Flavio Silva <flaviosp15@outlook.com>
2023-01-16 02:58:32 -0300

    :sparkles: feat: Optimizing the rendering of board

--- src/index.js
+++ src/index.js
@@ -12,29 +12,23 @@ function Square(props) {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+    return <Square key={i} value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+  }
+
+  renderBoard() {
+    const arr = Array(3).fill(null);
+    const boardRows = arr;
+    const boardColumns = arr;
+    let square = 0;
+    return boardRows.map((_, i) => (
+      <div key={i} className="board-row">
+        {boardColumns.map(_ => this.renderSquare(square++))}
+      </div>
+    ));
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    return <div>{this.renderBoard()}</div>;
   }
 }
 
